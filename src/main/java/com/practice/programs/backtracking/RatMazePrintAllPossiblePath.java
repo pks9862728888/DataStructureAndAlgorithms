b8ca100ab363fda90d05e12493589a9a53a2d1a9
pks9862728888 <pks9862728888@gmail.com>
2022-04-03 12:58:06 +0530

    Optimized program for RatMaze

--- src/main/java/com/practice/programs/backtracking/RatMazePrintAllPossiblePath.java
+++ src/main/java/com/practice/programs/backtracking/RatMazePrintAllPossiblePath.java
@@ -5,52 +5,52 @@ import com.utils.Printer;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * Optimal solution reached
+ */
 public class RatMazePrintAllPossiblePath implements Printer {
-
-    private static final int TARGET_X = 2;
-    private static final int TARGET_Y = 2;
 
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> m = new ArrayList<>();
+//        m.add(new ArrayList<>(Arrays.asList(1, 1)));
+//        m.add(new ArrayList<>(Arrays.asList(1, 1)));
         m.add(new ArrayList<>(Arrays.asList(1, 0, 0, 0, 0, 0)));
         m.add(new ArrayList<>(Arrays.asList(1, 0, 0, 0, 0, 0)));
         m.add(new ArrayList<>(Arrays.asList(1, 0, 1, 1, 1, 0)));
         m.add(new ArrayList<>(Arrays.asList(1, 0, 0, 1, 1, 0)));
         m.add(new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1, 0)));
         m.add(new ArrayList<>(Arrays.asList(1, 0, 0, 0, 0, 0)));
-        System.out.println(findAllPaths(m));
+        System.out.println(findAllPaths(m, 2, 2));
     }
 
-    public static ArrayList<String> findAllPaths(ArrayList<ArrayList<Integer>> m) {
+    public static ArrayList<String> findAllPaths(ArrayList<ArrayList<Integer>> m, int tx, int ty) {
         ArrayList<String> paths = new ArrayList<>();
-        findPaths(m, 0, 0, m.size(), paths, new ArrayList<>(), "");
+        ArrayList<ArrayList<Integer>> visited = new ArrayList<>();  // Optimal solution is to make a copy of matrix as visited
+        for (ArrayList<Integer> rows: m) {
+            visited.add(new ArrayList<>(rows));
+        }
+        findPaths(m, 0, 0, tx, ty, m.size(), paths, visited, "");
         return paths;
     }
     private static void findPaths(
-            ArrayList<ArrayList<Integer>> m, int sx, int sy, int n,
-            ArrayList<String> paths, ArrayList<Integer[]> tp, String cp) {
-        if (sx == TARGET_X && sy == TARGET_Y && m.get(sx).get(sy) == 1) {
+            ArrayList<ArrayList<Integer>> m, int sx, int sy, int tx, int ty, int n,
+            ArrayList<String> paths, ArrayList<ArrayList<Integer>> visited, String cp) {
+        if (sx == tx && sy == ty && m.get(sx).get(sy) == 1) {
             paths.add(cp);
             return;
         }
-        if (isValid(m, sx, sy, n, tp)) {
-            Integer[] ob = {sx, sy};
-            tp.add(ob);
-            findPaths(m, sx + 1, sy, n, paths, tp, cp + "D");
-            findPaths(m, sx, sy + 1, n, paths, tp, cp + "R");
-            findPaths(m, sx, sy - 1, n, paths, tp, cp + "L");
-            findPaths(m, sx - 1, sy, n, paths, tp, cp + "U");
-            tp.remove(ob);
+        if (isValid(visited, sx, sy, n)) {
+            visited.get(sx).set(sy, 0);
+            findPaths(m, sx + 1, sy, tx, ty, n, paths, visited, cp + "D");
+            findPaths(m, sx, sy + 1, tx, ty, n, paths, visited, cp + "R");
+            findPaths(m, sx, sy - 1, tx, ty, n, paths, visited, cp + "L");
+            findPaths(m, sx - 1, sy, tx, ty, n, paths, visited, cp + "U");
+            visited.get(sx).set(sy, 1);
         }
     }
-    private static boolean isValid(ArrayList<ArrayList<Integer>> m, int sx, int sy, int n, ArrayList<Integer[]> tp) {
-        if (sx < 0 || sy < 0 || sx == n || sy == n || m.get(sx).get(sy) == 0) {
+    private static boolean isValid(ArrayList<ArrayList<Integer>> visited, int sx, int sy, int n) {
+        if (sx < 0 || sy < 0 || sx == n || sy == n || visited.get(sx).get(sy) == 0) {
             return false;
-        }
-        for (Integer[] nd: tp) {
-            if (nd[0] == sx && nd[1] == sy) {
-                return false;
-            }
         }
         return true;
     }
