47a0e99550045ca7e42338808e54000a5f12bc50
pks9862728888 <pks9862728888@gmail.com>
2022-04-04 23:48:49 +0530

    Optimized n queen problem

--- src/main/java/com/practice/programs/backtracking/nqueen/NQueenProblemAllPossibleCombinations.java
+++ src/main/java/com/practice/programs/backtracking/nqueen/NQueenProblemAllPossibleCombinations.java
@@ -3,7 +3,6 @@ package com.practice.programs.backtracking.nqueen;
 import com.utils.Printer;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * Optimized solution
@@ -13,76 +12,72 @@ public class NQueenProblemAllPossibleCombinations implements Printer {
 
     public static void main(String[] args) {
 //        placeNQueen(3);
-        placeNQueen(4);
+        Printer.printMatrixes(nQueens(4), 4);
     }
 
-    private static void placeNQueen(int n) {
-        int[] rowP = new int[n];
-        int[] diag1 = new int[2 * n];
-        int[] diag2 = new int[2 * n];
-        Arrays.fill(rowP, -1);
-        Arrays.fill(diag1, -1);
-        Arrays.fill(diag2, -1);
-        ArrayList<ArrayList<Integer>> placements = new ArrayList<>();
-        if (placeNQueen(placements, rowP, diag1, diag2, 0, n)) {
-            Printer.printMatrixes(placements, n);
-        } else {
-            System.out.println("Placement not possible");
+    public static ArrayList<ArrayList<Integer>> nQueens(int n) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+        ArrayList<Integer> queensInRows = new ArrayList<>();
+        ArrayList<Integer> queensInDiag1 = new ArrayList<>();
+        ArrayList<Integer> queensInDiag2 = new ArrayList<>();
+        addToList(queensInRows, n, -1);
+        addToList(queensInDiag1, 2 * n, -1);
+        addToList(queensInDiag2, 2 * n, -1);
+        nQueensHelper(0, n, ans, queensInRows, queensInDiag1, queensInDiag2);
+
+        return ans;
+    }
+
+    public static void nQueensHelper(int col, int n, ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> qr,
+                                     ArrayList<Integer> qd1, ArrayList<Integer> qd2) {
+        // If all the queens have been placed
+        if (col == n) {
+            addSolution(n, ans, qr);
+            return;
         }
-    }
 
-    private static boolean placeNQueen(ArrayList<ArrayList<Integer>> placements, int[] rp, int[] dp1, int[] dp2, int colIdx, int n) {
-        if (colIdx >= n) {
-            ArrayList<Integer> al = new ArrayList<>();
-            for (int i = 0; i < n; i++) { // i = row, rp[i] contains colIdx
-                for (int j = 0; j < n; j++) {
-                    if (j == rp[i]) {
-                        al.add(1);
-                    } else {
-                        al.add(0);
-                    }
-                }
+        // Try all the rows for column col
+        for (int row = 0; row < n; row++) {
+            // If it's not safe to place queen here
+            if ((qr.get(row) != -1) || (qd1.get(col - row + n - 1) != -1) || (qd2.get(col + row) != -1)) {
+                continue;
             }
-            placements.add(al);
-            return true;
+
+            // Mark queen placed
+            qr.set(row, col);
+            qd1.set(col - row + n - 1, col);
+            qd2.set(col + row, col);
+
+            // Try placing the remaining queens
+            nQueensHelper(col + 1, n, ans, qr, qd1, qd2);
+
+            // Un-mark the cell to backtrack
+            qr.set(row, -1);
+            qd1.set(col - row + n - 1, -1);
+            qd2.set(col + row, -1);
         }
-        boolean placed = false;
-        for (int i = 0; i < n; i++) {  // row
-            if (isSafeToPlaceQueen(rp, dp1, dp2, i, colIdx, n)) {
-                int[] rpCopy = Arrays.copyOf(rp, rp.length);
-                int[] dp1Copy = Arrays.copyOf(dp1, dp1.length);
-                int[] dp2Copy = Arrays.copyOf(dp2, dp2.length);
-                rpCopy[i] = colIdx;
-                dp1Copy[i + colIdx] = colIdx;
-                dp2Copy[colIdx - i + n - 1] = colIdx;
-                if (placeNQueen(placements, rpCopy, dp1Copy, dp2Copy, colIdx + 1, n)) {
-                    placed = true;
+    }
+
+    public static void addSolution(int n, ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> queensInRows) {
+        ArrayList<Integer> currentAnswer = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (queensInRows.get(j) == i) {
+                    currentAnswer.add(1);
                 } else {
-                    rpCopy[i] = -1;
-                    dp1Copy[i + colIdx] = -1;
-                    dp2Copy[colIdx - i + n - 1] = -1;
+                    currentAnswer.add(0);
                 }
             }
         }
-        return placed;
+
+        ans.add(currentAnswer);
     }
 
-    private static boolean isSafeToPlaceQueen(int[] rp, int[] dp1, int[] dp2, int rowIdx, int colIdx, int n) {
-        if (rp[rowIdx] != -1) { // check whether row contains queen
-            return false;
+    private static void addToList(ArrayList<Integer> al, int n, int value) {
+        for (int i = 0; i < n; i++) {
+            al.add(value);
         }
-        for (int i = 0; i < n; i++) {  // check whether column contains queen
-            if (rp[i] == colIdx) {
-                return false;
-            }
-        }
-
-        // upper left diagonal
-        if (dp1[rowIdx + colIdx] != -1) {
-            return false;
-        }
-
-        // lower left diagonal
-        return dp2[colIdx - rowIdx + n - 1] == -1;
     }
 }
