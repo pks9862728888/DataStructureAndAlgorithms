07347405e5a1645111aa60882f6a0c2aca7fb6f5
pks9862728888 <pks9862728888@gmail.com>
2022-05-15 23:00:50 +0530

    Implemented linked list sorting

--- src/main/java/com/demo/datastructures/linkedlist/LinkedList.java
+++ src/main/java/com/demo/datastructures/linkedlist/LinkedList.java
@@ -1,13 +1,36 @@
 package com.demo.datastructures.linkedlist;
 
-public class LinkedList<T> {
+import java.util.Objects;
 
-    static class Node<T> {
+public class LinkedList<T extends Comparable<T>> {
+
+    static class Node<T extends Comparable<T>> {
         T data;
         Node<T> next;
 
         public Node(T data) {
             this.data = data;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node<?> node = (Node<?>) o;
+            return Objects.equals(data, node.data) && Objects.equals(next, node.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(data, next);
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    ", next=" + next +
+                    '}';
         }
     }
 
@@ -109,7 +132,7 @@ public class LinkedList<T> {
         Node<T> slow = head;
         Node<T> fast = head;
         int n = 0;
-        while(slow != null && fast != null) {
+        while (slow != null && fast != null) {
             slow = slow.next;
             fast = fast.next;
             n++;
@@ -173,5 +196,158 @@ public class LinkedList<T> {
         }
         str.append("]");
         return str.toString();
+    }
+
+    public void printRecursively() {
+        printRecursively(head);
+        System.out.println();
+    }
+
+    private void printRecursively(Node<T> head) {
+        if (head != null) {
+            System.out.print(head.data + " -> ");
+            printRecursively(head.next);
+        }
+    }
+
+    public int insertRecursively(int pos, T data) {
+        if (pos == 0) {
+            Node<T> node = new Node<>(data);
+            node.next = head;
+            if (head == null) {
+                tail = node;
+            }
+            head = node;
+            length++;
+            return 1;
+        }
+        return insertRecursively(pos, data, head);
+    }
+
+    private int insertRecursively(int pos, T data, Node<T> prevNode) {
+        if (prevNode == null) {
+            return 0;
+        } else {
+            if (pos - 1 == 0) {
+                Node<T> node = new Node<>(data);
+                node.next = prevNode.next;
+                prevNode.next = node;
+                length++;
+                return 1;
+            }
+            return insertRecursively(pos - 1, data, prevNode.next);
+        }
+    }
+
+    public Node<T> deleteRecursively(int pos) {
+        if (head == null) {
+            return null;
+        } else if (pos == 0) {
+            Node<T> deletedNode = head;
+            head = head.next;
+            deletedNode.next = null;
+            length--;
+            if (head == null) {
+                tail = null;
+            }
+            return deletedNode;
+        } else {
+            return deleteRecursively(head, pos);
+        }
+    }
+
+    private Node<T> deleteRecursively(Node<T> node, int pos) {
+        if (node == null && pos > 0) {
+            return null;
+        } else {
+            if (pos - 1 == 0) {
+                if (node.next != null) {
+                    Node<T> deletedNode = node.next;
+                    node.next = node.next.next;
+                    if (tail.data.equals(deletedNode.data) && deletedNode.next == null) {
+                        tail = node;
+                    }
+                    deletedNode.next = null;
+                    length--;
+                    return deletedNode;
+                } else {
+                    return null;
+                }
+            }
+            return deleteRecursively(node.next, pos - 1);
+        }
+    }
+
+    public void reverseRecursively() {
+        Node<T>[] nodeArr = reverseRecursively(head);
+        head = nodeArr[0];
+    }
+
+    private Node<T>[] reverseRecursively(Node<T> head) {
+        if (head == null || head.next == null) {
+            return new Node[]{head, head};
+        }
+        Node<T>[] result = reverseRecursively(head.next);
+        result[1].next = head;
+        result[1] = head;
+        result[1].next = null;
+        return result;
+    }
+
+    public void mergeSort() {
+        head = mergeSort(head);
+    }
+
+    private Node<T> mergeSort(Node<T> head) {
+        if (head != null && head.next != null) {
+            Node<T> mid = findMidOfLL(head);
+
+            // Break the connection to next
+            Node<T> rightHalfHead = mid.next;
+            mid.next = null;
+
+            // Recursively call merge in right and left half and merge the result
+            head = mergeSort(head);
+            rightHalfHead = mergeSort(rightHalfHead);
+            head = mergeOfMergeSort(head, rightHalfHead);
+        }
+        return head;
+    }
+
+    private Node<T> mergeOfMergeSort(Node<T> h1, Node<T> h2) {
+        Node<T> head = new Node<>(null);
+        Node<T> curr = head;
+        while (h1 != null || h2 != null) {
+            if (h1 == null) {
+                curr.next = h2;
+                break;
+            } else if (h2 == null) {
+                curr.next = h1;
+                break;
+            } else if (h1.data.compareTo(h2.data) <= 0) {
+                curr.next = h1;
+                curr = curr.next;
+                h1 = h1.next;
+                curr.next = null;
+            } else {
+                curr.next = h2;
+                curr = curr.next;
+                h2 = h2.next;
+                curr.next = null;
+            }
+        }
+
+        return head.next;
+    }
+
+    private Node<T> findMidOfLL(Node<T> head) {
+        Node<T> slow = head;
+        Node<T> fast = head;
+
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
     }
 }
--- src/main/java/com/demo/datastructures/linkedlist/LinkedListDemo.java
+++ src/main/java/com/demo/datastructures/linkedlist/LinkedListDemo.java
@@ -22,19 +22,50 @@ public class LinkedListDemo {
 //        System.out.println(linkedList);
 //        System.out.println(linkedList.getLast());
 
-        LinkedList<Integer> l1 = new LinkedList<>();
-        l1.insert(0, 1);
-        l1.insert(1, 2);
-        l1.insert(2, 2);
-        l1.insert(3, 1);
-        System.out.println(l1.isPalindrome());
+//        LinkedList<Integer> l1 = new LinkedList<>();
+//        l1.insertRecursively(0, 1);
+//        l1.insertRecursively(1, 2);
+//        l1.insertRecursively(2, 2);
+//        l1.insertRecursively(3, 4);
+//        l1.reverseRecursively();
+//        l1.printRecursively();
+//        System.out.println(l1.isPalindrome());
+//        l1.printRecursively();
+//
+//        // Deleting
+//        System.out.println("DN: " + l1.deleteRecursively(4));
+//        l1.printRecursively();
+//        System.out.println(l1.getLast());
+//        System.out.println("-------------------------");
+//        System.out.println("DN: " + l1.deleteRecursively(3));
+//        l1.printRecursively();
+//        System.out.println(l1.getLast());
+//        System.out.println("-------------------------");
+//        System.out.println("DN: " + l1.deleteRecursively(1));
+//        l1.printRecursively();
+//        System.out.println(l1.getLast());
+////        System.out.println(l1.deleteRecursively(1));
+//        System.out.println("-------------------------");
+//        System.out.println("DN: " + l1.deleteRecursively(0));
+//        l1.printRecursively();
+//        System.out.println(l1.getLast());
+//        System.out.println(l1.size());
+//
+//        LinkedList<Integer> l2 = new LinkedList<>();
+//        l2.insert(0, 1);
+//        l2.insert(1, 2);
+//        l2.insert(2, 3);
+//        l2.insert(3, 2);
+//        l2.insert(4, 1);
+//        System.out.println(l2.isPalindrome());
 
-        LinkedList<Integer> l2 = new LinkedList<>();
-        l2.insert(0, 1);
-        l2.insert(1, 2);
-        l2.insert(2, 3);
-        l2.insert(3, 2);
-        l2.insert(4, 1);
-        System.out.println(l2.isPalindrome());
+        LinkedList<Integer> l1 = new LinkedList<>();
+        l1.insertRecursively(0, 1);
+        l1.insertRecursively(1, 4);
+        l1.insertRecursively(2, 3);
+        l1.insertRecursively(3, 6);
+        l1.insertRecursively(4, 5);
+        l1.mergeSort();
+        l1.printRecursively();
     }
 }
