b84944ce2b8e99fde5912f2e8b71c231887a03bf
pks9862728888 <pks9862728888@gmail.com>
2022-07-31 23:19:19 +0530

    Implemented min squares to represent no

--- src/main/java/com/demo/dp/MinSquaresToRepresentNumber.java
+++ src/main/java/com/demo/dp/MinSquaresToRepresentNumber.java
@@ -7,6 +7,7 @@ public class MinSquaresToRepresentNumber {
     /**
      * Given an integer N, find and return the count of minimum numbers required to
      * represent N as a sum of squares.
+     * TC: O(n Root (n)) AS: O(n)
      **/
     public static void main(String[] args) {
         System.out.println("3: " + minCount(3));
@@ -14,32 +15,31 @@ public class MinSquaresToRepresentNumber {
         System.out.println("12: " + minCount(12));
     }
 
-	public static int minCount(int n) {
+    public static int minCount(int n) {
         if (n <= 0) {
             return 0;
         } else if (n == 1) {
             return 1;
         }
-		int[] dp = new int[n + 1];
+        int[] dp = new int[n + 1];
         Arrays.fill(dp, -1);
         dp[0] = 0;
         dp[1] = 1;
-        
+
         for (int i = 2; i <= n; i++) {
             getMinCount(i, dp);
         }
-        
+
         return dp[n];
-	}
-    
+    }
+
     private static int getMinCount(int n, int[] dp) {
-        int maxNo = (int) Math.sqrt(n);
         int min = Integer.MAX_VALUE;
-        for (int i = 1; i <= maxNo; i++) {
+        for (int i = 1; i * i <= n; i++) {
             int res = n - (i * i);
             if (dp[res] == -1) {
                 dp[res] = getMinCount(res, dp);
-            } 
+            }
             min = Math.min(min, dp[res]);
         }
         dp[n] = min + 1;
