45af51d788a4dfd479971c6a37e9602b1a1325a3
pks9862728888 <pks9862728888@gmail.com>
2022-08-06 15:52:58 +0530

    Implemented ways to make coin change

--- src/main/java/com/demo/dp/WaysToMakeCoinChange.java
+++ src/main/java/com/demo/dp/WaysToMakeCoinChange.java
@@ -7,6 +7,9 @@ public class WaysToMakeCoinChange {
      * You need to figure out the total number of ways W,
      * in which you can make the change for Value V using coins of denominations D.
      * Return 0 if the change isn't possible.
+     *
+     * TC: O(mn)
+     * SC: O(mn)
      */
     public static void main(String[] args) {
         // Number of ways are - 4 total i.e. (1,1,1,1), (1,1, 2), (1, 3) and (2, 2).
