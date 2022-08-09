52b481e538ced9407a11f11fa91335fe57002bd6
pks9862728888 <pks9862728888@gmail.com>
2022-08-10 01:43:51 +0530

    Implemented SubsetSumEqualToKPresent

--- src/main/java/com/demo/dp/SubsetSumEqualToKPresent.java
+++ src/main/java/com/demo/dp/SubsetSumEqualToKPresent.java
@@ -1,5 +1,7 @@
 package com.demo.dp;
 
+import java.util.Arrays;
+
 public class SubsetSumEqualToKPresent {
 
     /**
@@ -8,6 +10,41 @@ public class SubsetSumEqualToKPresent {
      */
     public static void main(String[] args) {
         System.out.println(isSubsetPresentRecursive(new int[] {4, 2, 5, 6, 7}, 14));
+        System.out.println(isSubsetPresentIterative(new int[] {4, 2, 5, 6, 7}, 14));
+    }
+
+    static boolean isSubsetPresentIterative(int[] arr, int sum) {
+        int[][] dp = new int[arr.length][sum + 1];
+
+        for (int[] a: dp) {
+            Arrays.fill(a, -1);
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for(int s = 0; s <= sum; s++) {
+                dp[i][s] = exists(arr, i, s, dp) ? 1 : 0;
+                if (s == sum && dp[i][s] == 1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean exists(int[] arr, int i, int sum, int[][] dp) {
+        if (sum == 0) {
+            return true;
+        } else if (sum < 0 || i == arr.length) {
+            return false;
+        } else if (dp[i][sum] != -1) {
+            return dp[i][sum] == 1;
+        }
+        boolean found = exists(arr, i + 1, sum, dp);      // Including ith element
+        if (!found) {
+            found = exists(arr, i + 1, sum - arr[i], dp); // Excluding ith element
+        }
+        return found;
     }
 
     static boolean isSubsetPresentRecursive(int[] arr, int sum) {
