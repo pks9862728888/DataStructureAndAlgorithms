63217ef43574e177b9af3f8cbd9ab1b064b0ceb5
pks9862728888 <pks9862728888@gmail.com>
2022-08-31 21:49:29 +0530

    Stashing changes

---
+++ src/main/java/com/practice/programs/number/ReplaceZeroWithFive.java
@@ -0,0 +1,22 @@
+package com.practice.programs.number;
+
+public class ReplaceZeroWithFive {
+
+    public static void main(String[] args) {
+        int n = 1060;
+        int res = 0;
+        int multiplier = 1;     // To prevent number from getting reversed
+
+        while (n > 0) {
+            int rem = n % 10;    // Find the reminder
+            if (rem == 0) {      // Question was to replace all 0 with 5, so we replace 0 with 5
+                rem = 5;
+            }
+            res = res + rem * multiplier;  // Logic to re-construct number without getting reversed
+            n /= 10;
+            multiplier *= 10;
+        }
+
+        System.out.println(res);
+    }
+}
