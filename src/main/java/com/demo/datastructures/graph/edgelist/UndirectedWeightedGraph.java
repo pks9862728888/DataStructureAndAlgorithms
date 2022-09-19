2282d5dc6d3c98cad03ff94d193430033a259b99
pks9862728888 <pks9862728888@gmail.com>
2022-09-20 00:36:00 +0530

    Implemented min spanning tree using Kruskals algorithm

---
+++ src/main/java/com/demo/datastructures/graph/edgelist/UndirectedWeightedGraph.java
@@ -0,0 +1,74 @@
+package com.demo.datastructures.graph.edgelist;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class UndirectedWeightedGraph {
+
+    @AllArgsConstructor
+    @ToString
+    public static class Edge {
+        int v1;
+        int v2;
+        double weight;
+    }
+
+    int noOfVertex;
+    int e;
+    private Edge[] edgeList;
+    int edgeIdx = 0;
+
+    public UndirectedWeightedGraph(int noOfVertices, int noOfEdges) {
+        this.noOfVertex = noOfVertices;
+        this.e = noOfEdges;
+        this.edgeList = new Edge[noOfEdges];
+    }
+
+    public void addEdge(int v1, int v2, double weight) {
+        this.edgeList[edgeIdx++] = new Edge(v1, v2, weight);
+    }
+
+    /**
+     * No cycle
+     * Min sum of weights of edges
+     */
+    public Edge[] generateMinSpanningTreeUsingKruskalsAndUnionFindAlgorithm() {
+        Arrays.sort(edgeList, Comparator.comparingDouble(o -> o.weight));
+        Edge[] output = new Edge[noOfVertex - 1];  // length = n - 1
+        int outIdx = 0;
+        int edgeListIdx = 0;
+
+        // Generate parent array and fill it with self idx
+        int[] parentArray = new int[noOfVertex];
+        for (int i = 0; i < parentArray.length; i++) {
+            parentArray[i] = i;
+        }
+
+        while (outIdx < output.length) {
+            Edge currentEdge = edgeList[edgeListIdx++];
+            int topParentV1 = getTopParent(parentArray, currentEdge.v1);
+            int topParentV2 = getTopParent(parentArray, currentEdge.v2);
+
+            if (topParentV1 != topParentV2) {
+                // Belong to different component, so safe to add
+                output[outIdx++] = currentEdge;
+
+                // Update top parent
+                parentArray[topParentV1] = topParentV2;
+            }
+        }
+
+        // Return edge list
+        return output;
+    }
+
+    private int getTopParent(int[] parentArray, int vertex) {
+        while (vertex != parentArray[vertex]) {
+            vertex = parentArray[vertex];
+        }
+        return vertex;
+    }
+}
