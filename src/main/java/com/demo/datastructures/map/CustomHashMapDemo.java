826a4c45e1371db9f38bf8cf0a36b64d095bf748
pks9862728888 <pks9862728888@gmail.com>
2022-11-20 12:29:14 +0530

    Implemented treemap demo

---
+++ src/main/java/com/demo/datastructures/map/CustomHashMapDemo.java
@@ -0,0 +1,26 @@
+package com.demo.datastructures.map;
+
+public class CustomHashMapDemo {
+
+    public static void main(String[] args) {
+        loadFactorDemo();
+    }
+
+    private static void loadFactorDemo() {
+        CustomHashMap<Integer, Integer> map = new CustomHashMap<>();
+        map.put(1, 1);
+        System.out.println(map.getCurrentLoadFactor() + " " + map.size());
+        map.put(1, 2);
+        System.out.println(map.getCurrentLoadFactor() + " " + map.size());
+        map.put(2, 2);
+        System.out.println(map.getCurrentLoadFactor() + " " + map.size());
+        map.put(3, 3);
+        System.out.println(map.getCurrentLoadFactor() + " " + map.size());
+        map.put(4, 4);
+        System.out.println(map.getCurrentLoadFactor() + " " + map.size());
+        System.out.println(map);
+        map.delete(1);
+        System.out.println(map);
+        System.out.println(map.getCurrentLoadFactor() + " " + map.size());
+    }
+}
