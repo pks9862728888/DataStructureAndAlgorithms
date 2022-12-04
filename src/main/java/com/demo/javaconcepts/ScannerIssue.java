8556ba82cf76345d92ccd58307cfce94c545eae0
pks9862728888 <pks9862728888@gmail.com>
2022-12-04 13:37:24 +0530

    Scanner class issue highlighted

---
+++ src/main/java/com/demo/javaconcepts/ScannerIssue.java
@@ -0,0 +1,17 @@
+package com.demo.javaconcepts;
+
+import java.util.Scanner;
+
+public class ScannerIssue {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Highlighting nextInt() does not consume trailing new line character
+        System.out.println("Enter an integer and then a string...");
+        System.out.print("Integer: " + sc.nextInt());
+        System.out.print("String: " + sc.nextLine());
+
+        sc.close();
+    }
+}
