1ae3674dc96e129f886e6d1dccf10d5885e411ae
pks9862728888 <pks9862728888@gmail.com>
2023-03-12 14:06:00 +0530

    Implemented min heap

---
+++ src/main/java/com/demo/datastructures/heaps/MinHeapDemo.java
@@ -0,0 +1,16 @@
+package com.demo.datastructures.heaps;
+
+class MinHeapDemo {
+
+    public static void main(String[] args) {
+        MinHeap<String> minHeap = new MinHeap<>(5);
+        minHeap.add(10, "Ten");
+        minHeap.add(20, "Twenty");
+        minHeap.add(30, "Thirty");
+        minHeap.add(40, "Forty");
+        minHeap.add(50, "Fifty");
+        minHeap.add(5, "Five");
+        minHeap.add(2, "Two");
+        System.out.println(minHeap);
+    }
+}
