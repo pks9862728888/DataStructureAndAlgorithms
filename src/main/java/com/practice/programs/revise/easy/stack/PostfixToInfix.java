c3eae8a11f13a5ab54d25e1b27300810e74043e7
hackedlings <pks9862728888@gmail.com>
2023-08-28 22:18:32 +0530

    Added postfix to prefix and infix conversion

---
+++ src/main/java/com/practice/programs/revise/easy/stack/PostfixToInfix.java
@@ -0,0 +1,37 @@
+package com.practice.programs.revise.easy.stack;
+
+import java.util.Stack;
+
+/**
+ * TC: (n) AS: O(n)
+ */
+public class PostfixToInfix {
+
+    public static void main(String[] args) {
+        System.out.println(postToInfix("ab+c*de*+")); // (((a + b) * c) + (d + e))
+        System.out.println(postToInfix("ab+c+"));     // ((a+b)+c)
+    }
+
+    public static String postToInfix(String exp) {
+        // Create a stack for storing operands and result of expression
+        // For each element in expression
+        //    If letter or digit add to stack
+        //    If operator
+        //        second_ele = pop()
+        //        first_ele = pop()
+        //        add to stack = (first_ele operator second_ele)
+        // When all done, return st.pop()
+        Stack<String> st = new Stack<>();
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                st.push(String.valueOf(ch));
+            } else {
+                String secondEle = st.pop();
+                String firstEle = st.pop();
+                st.push("(" + firstEle + ch + secondEle + ")");
+            }
+        }
+        return st.pop();
+    }
+}
---
+++ src/main/java/com/practice/programs/revise/easy/stack/PostfixToPrefix.java
@@ -0,0 +1,37 @@
+package com.practice.programs.revise.easy.stack;
+
+import java.util.Stack;
+
+/**
+ * TC: (n) AS: O(n)
+ */
+public class PostfixToPrefix {
+
+    public static void main(String[] args) {
+        System.out.println(postfixToPrefix("abc/d*e*-f+"));  // -a+*/bc*def
+    }
+
+    public static String postfixToPrefix(String exp) {
+        // Create stack for storing the expression
+        // For each element in expression
+        // If it is letter or digit
+        //    add to stack
+        // If it is operator
+        //    second_ele = st.pop()
+        //    first_ele = st.pop()
+        //    push to stack = operand first_ele second_ele
+        // when end of exp is reached return st.pop()
+        Stack<String> st = new Stack<>();
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                st.push(String.valueOf(ch));
+            } else {
+                String se = st.pop();
+                String fe = st.pop();
+                st.push(ch + fe + se);
+            }
+        }
+        return st.pop();
+    }
+}
