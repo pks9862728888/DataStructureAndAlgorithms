11ba2d059224e13dc1e98b83ec498b797944b49f
hackedlings <pks9862728888@gmail.com>
2023-09-09 17:37:56 +0530

    Solved some DSA problems

--- src/main/java/com/practice/programs/easy/twopointer/RemoveDuplicateElementsSuchThatEachElementOccursAtMostTwice.java
+++ src/main/java/com/practice/programs/easy/twopointer/RemoveDuplicateElementsSuchThatEachElementOccursAtMostTwice.java
@@ -8,21 +8,19 @@ public class RemoveDuplicateElementsSuchThatEachElementOccursAtMostTwice {
     // 2 2 2 3 4 4 9 -> 2 2 3 4 4 9 (6)
 
     static int removeDuplicatesFromSortedArrayII(int n, int[] arr) {
-        // Keep a pointer pointed to 1st ele, and freq = 1 and prev = arr[0]
+        // Keep a pointer pointed to 1st ele, and freq = 1
         // Iterate over from 1 to n - 1
         // If curr == prev, freq++
         //    If freq == 2, do not do anything
         //    If freq < 2, then put arr[++pointer] = currEle
-        // If currr != prev, freq = 1, prev = curr, arr[++pointer] = curr
+        // If curr != prev, freq = 1, arr[++pointer] = curr
         int frontIdx = 0;
-        int prev = arr[0];
         int freq = 1;
         for (int i = 1; i < n; i++) {
-            if (arr[i] == prev) {
+            if (arr[i] == arr[i - 1]) {
                 freq++;
             } else {
                 freq = 1;
-                prev = arr[i];
             }
             if (freq <= 2) {
                 arr[++frontIdx] = arr[i];
