70a5b859dd7710d362d29f56be917de9a0eca33a
hackedlings <pks9862728888@gmail.com>
2023-09-10 10:40:12 +0530

    Solved some DSA problems

---
+++ src/main/java/com/practice/programs/easy/tree/LevelOrderTraversal.java
@@ -0,0 +1,69 @@
+package com.practice.programs.easy.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class LevelOrderTraversal {
+
+    public List<List<Integer>> binaryTreeLevelOrderTraversal(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        getLevelOrderResNonRecursive(root, res);
+        // getLevelOrderOutputRecursivePreOrder(root, res, 0);
+        return res;
+    }
+
+    /**
+     * TC: O(n) AS: O(2^h - 1)
+     */
+    private void getLevelOrderResNonRecursive(TreeNode node, List<List<Integer>> res) {
+        if (node == null) {
+            return;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(node);
+        while (!q.isEmpty()) {
+            List<Integer> levelOrderEle = new ArrayList<>();
+            res.add(levelOrderEle);
+            int levelOrderEleSize = q.size();
+            while (levelOrderEleSize-- > 0) {
+                TreeNode curr = q.poll();
+                levelOrderEle.add((int) curr.val);
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+
+    /**
+     * TC: O(n), AS: O(h) or O (log n)
+     */
+    private void getLevelOrderOutputRecursivePreOrder(TreeNode node, List<List<Integer>> res, int depth) {
+        if (node == null) {
+            return;
+        }
+        if (res.size() < depth + 1) {
+            res.add(new ArrayList<>());
+        }
+        res.get(depth).add((int) node.val);
+        getLevelOrderOutputRecursivePreOrder(node.left, res, depth + 1);
+        getLevelOrderOutputRecursivePreOrder(node.right, res, depth + 1);
+    }
+}
+
+class TreeNode {
+    public long val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode (long x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
---
+++ src/main/java/com/practice/programs/revise/medium/algorithms/SeiveOrEratothenesCountNoOrPrimeLessThanN.java
@@ -0,0 +1,37 @@
+package com.practice.programs.revise.medium.algorithms;
+
+
+import java.util.Arrays;
+
+/**
+ * TC: O(n), AS: O(1)
+ * <a href="https://leetcode.com/problems/count-primes/description/">Practice link</a>
+ */
+public class SeiveOrEratothenesCountNoOrPrimeLessThanN {
+
+    public int countPrimes(int n) {
+        // Use array to store the prime numbers
+        // For each prime no in array upto sqrt(n), mark all multiples of curr no >= no * no as non primt
+        boolean[] res = new boolean[n];
+        Arrays.fill(res, true);
+        res[0] = false;
+        res[1] = false;
+        for (int i = 2; i * i < n; i++) {
+            if (res[i]) {
+                // Mark all multiples of prime no as non prime
+                for (int j = i * i; j < n; j += i) {
+                    res[j] = false;
+                }
+            }
+        }
+
+        // Count no of prime numbers in array
+        int c = 0;
+        for (int i = 2; i < n; i++) {
+            if (res[i]) {
+                c++;
+            }
+        }
+        return c;
+    }
+}
