623dae8ca77123be1bfd87ff2f47ab0d403a3609
hackedlings <pks9862728888@gmail.com>
2023-09-14 22:37:26 +0530

    Solved some DSA problems

---
+++ src/main/java/com/practice/programs/revise/easy/array/FindLongestSubstringWithAtmostKDistinctCharacter.java
@@ -0,0 +1,38 @@
+package com.practice.programs.revise.easy.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TC: O(n), AS: O(n)
+ * Sliding window approach of variable window size
+ */
+public class FindLongestSubstringWithAtmostKDistinctCharacter {
+
+    // abacd, 3 -> abac (4)
+    // Xyyzya, 3 -> yyzya (5)
+
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if (s.length() <= 1) {
+            return s.length();
+        }
+        Map<Character, Integer> freq = new HashMap<>();
+        int left = 0, right = 0;
+        int lengthLongestSubstring = 0;
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+            while (freq.size() > k) {
+                char leftCh = s.charAt(left);
+                freq.put(leftCh, freq.get(leftCh) - 1);
+                if (freq.get(leftCh) == 0) {
+                    freq.remove(leftCh);
+                }
+                left++;
+            }
+            lengthLongestSubstring = Math.max(lengthLongestSubstring, right - left + 1);
+            right++;
+        }
+        return lengthLongestSubstring;
+    }
+}
---
+++ src/main/java/com/practice/programs/revise/easy/array/FindLongestSubstringWithoutRepeatingCharacter.java
@@ -0,0 +1,40 @@
+package com.practice.programs.revise.easy.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TC: O(n) AS: O(n)
+ * Sliding window of variable size
+ */
+public class FindLongestSubstringWithoutRepeatingCharacter {
+
+    // aabcccbcb -> abc -> 3
+    // cdddddd -> cd -> 2
+
+    /**
+     * Keep left and right pointer
+     *    Iterate over the string and keep track of frequency of the elements in the window
+     *      While freq of right ch > 1, decrease window length (leftPointer++), and update frequency
+     *      Update maxChar length
+     */
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1) {
+            return s.length();
+        }
+        Map<Character, Integer> freq = new HashMap<>();
+        int left = 0, right = 0;
+        int longestSubstringLength = 1;
+        while (right < s.length()) {
+            char rightCh = s.charAt(right);
+            freq.put(rightCh, freq.getOrDefault(rightCh, 0) + 1);
+            while (freq.get(rightCh) > 1) {
+                freq.put(s.charAt(left), freq.get(s.charAt(left)) - 1);
+                left++;
+            }
+            longestSubstringLength = Math.max(longestSubstringLength, right - left + 1);
+            right++;
+        }
+        return longestSubstringLength;
+    }
+}
