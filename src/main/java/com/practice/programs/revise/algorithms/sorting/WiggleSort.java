3c219ec1aef45ef98640860f37e153f094ebeaa2
hackedlings <pks9862728888@gmail.com>
2023-09-17 12:10:45 +0530

    Implemented some sorting algorithms

---
+++ src/main/java/com/practice/programs/revise/algorithms/sorting/WiggleSort.java
@@ -0,0 +1,31 @@
+package com.practice.programs.revise.algorithms.sorting;
+
+import java.util.List;
+
+/**
+ * TC: O(n), AS: O(1)
+ * Sorts in wave like fashion
+ */
+class WiggleSort {
+
+    // 1 5 1 1 1 6 4 -> 1 5 1 1 1 6 4
+
+    static List<Integer> wiggleSort(int n, List<Integer>nums) {
+        for (int i = 1; i < n; i += 2) {
+            if (nums.get(i) < nums.get(i - 1)) {
+                swap(nums, i, i - 1);
+            }
+            if (i + 1 < n && nums.get(i)  < nums.get(i + 1)) {
+                swap(nums, i, i + 1);
+            }
+        }
+        return nums;
+    }
+
+    private static void swap(List<Integer> nums, int i, int j) {
+        Integer t = nums.get(i);
+        nums.set(i, nums.get(j));
+        nums.set(j, t);
+    }
+
+}
