5c18c51e1e28f58aa08426da85046228604cc4c8
hackedlings <pks9862728888@gmail.com>
2023-10-01 21:10:45 +0530

    Solved some DSA problems

---
+++ src/main/java/com/practice/programs/revise/medium/array/binarysearch/AggressiveCows.java
@@ -0,0 +1,45 @@
+package com.practice.programs.revise.medium.array.binarysearch;
+
+import java.util.Arrays;
+
+/**
+ * TC: O(nlog n + log n), AS: O(1)
+ * <a href="https://www.codingninjas.com/codestudio/problems/aggressive-cows_1082559?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf">Practice Link</a>
+ * Find maximum possible minimum distance
+ */
+class AggressiveCows {
+
+    // 1, 2, 3 -> ans = 2
+
+    public static int aggressiveCows(int []stalls, int k) {
+        Arrays.sort(stalls);
+        int st = 1;
+        int end = stalls[stalls.length - 1];
+        int minDistance = 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (canPlaceCows(stalls, mid, k)) {
+                minDistance = mid;
+                st = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return minDistance;
+    }
+
+    private static boolean canPlaceCows(int[] stalls, int distance, int maxCows) {
+        int prevStall = stalls[0];
+        int cowsCount = 1;
+        for (int i = 1; i < stalls.length; i++) {
+            if (stalls[i] - prevStall >= distance) {
+                prevStall = stalls[i];
+                cowsCount++;
+            }
+            if (cowsCount >= maxCows) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
