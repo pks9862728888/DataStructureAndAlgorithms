946f19921e9ff5a9e56c1b9d6012423ff52238c6
hackedlings <pks9862728888@gmail.com>
2023-10-05 21:12:07 +0530

    Solved some DSA problems

---
+++ src/main/java/com/practice/programs/revise/medium/stack/LongestValidParenthesis.java
@@ -0,0 +1,37 @@
+package com.practice.programs.revise.medium.stack;
+
+import java.util.Stack;
+
+/**
+ * TC: O(n) AS: O(n)
+ */
+public class LongestValidParenthesis {
+
+    // ()(()(()) -> ans = 6
+
+    static int longestValidParentheses(String str) {
+        // Create var to store result
+        // Create stack to store parenthesis idx
+        // If opening bracket -> push curr idx to stack
+        // If closing bracket
+        //      if stack is empty -> push curr idx to stack
+        //      if stack has valid parenthesis -> pop it -> get curr valid parenthesis = curr idx - previous invalid or begin -> update res
+        int longestValidParenthesis = 0;
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                st.push(i);
+            } else {
+                if (st.isEmpty() || str.charAt(st.peek()) != '(') {
+                    st.push(i);
+                } else {
+                    st.pop();
+                    int currLen = i - (st.isEmpty() ? -1 : st.peek());
+                    longestValidParenthesis = Math.max(currLen, longestValidParenthesis);
+                }
+            }
+        }
+        return longestValidParenthesis;
+    }
+}
