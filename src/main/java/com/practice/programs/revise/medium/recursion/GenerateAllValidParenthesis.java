6a237272cbff357735bd5666303cb7f75d09d327
hackedlings <pks9862728888@gmail.com>
2023-11-08 08:08:41 +0530

    Time complexity of generate all valid parenthesis

---
+++ src/main/java/com/practice/programs/revise/medium/recursion/GenerateAllValidParenthesis.java
@@ -0,0 +1,35 @@
+package com.practice.programs.revise.medium.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TC: O((1 / (n + 1)) * (2n C n)), which is approximated to: O((4 ^ n) / sqrt(n))
+ * AS: O(2n) -> 2n is recursion depth
+ */
+class GenerateAllValidParenthesis {
+
+    // n = 2, ans = [(()), ()()]
+    // Concept, we can't add closed braces count if countClosed > countOpened so far
+
+    ArrayList<String> balancedBraces(int n) {
+        ArrayList<String> res = new ArrayList<>();
+        generateBalancedBraces("", 0, 0, n, res);
+        return res;
+    }
+
+    private void generateBalancedBraces(
+            String parenthesis, int openCount, int closedCount, int n, List<String> res) {
+        if (parenthesis.length() == 2 * n) {
+            res.add(parenthesis);
+            return;
+        }
+        if (openCount < n) {
+            generateBalancedBraces(parenthesis + '(', openCount + 1, closedCount, n, res);
+        }
+        if (closedCount < openCount) {
+            generateBalancedBraces(parenthesis + ')', openCount, closedCount + 1, n, res);
+        }
+    }
+
+}
