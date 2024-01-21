0296c935ae2632e2cf6a3a76329aa5bf41f7745e
hackedlings <pks9862728888@gmail.com>
2024-01-21 11:20:18 +0530

    Solved some DSA problems

--- src/main/java/com/practice/programs/revise/medium/stack/ImplementMinStack.java
+++ src/main/java/com/practice/programs/revise/medium/stack/ImplementMinStack.java
@@ -6,34 +6,73 @@ import java.util.Stack;
  * TC: O(1) for all operations, AS: O(2n)
  */
 public class ImplementMinStack {
-    static Stack<Integer> stack = new Stack<>();
-    static Stack<Integer> min = new Stack<>();
+    private static Stack<Integer> st = new Stack<>();
+    private static Stack<Integer> minSt = new Stack<>();
 
     static void push(Integer e) {
-        stack.push(e);
-        if (min.isEmpty()) {
-            min.push(e);
-        } else {
-            if (e.compareTo(min.peek()) >= 0) {
-                min.push(min.peek());
-            } else {
-                min.push(e);
-            }
-        }
+        int min = minSt.isEmpty() ? e : Math.min(minSt.peek(), e);
+        st.push(e);
+        minSt.push(min);
     }
 
     static void pop() {
-        if (!stack.isEmpty()) {
-            stack.pop();
-            min.pop();
+        if (!st.isEmpty()) {
+            st.pop();
+            minSt.pop();
         }
     }
 
     static Integer findMin() {
-        if (min.isEmpty()) {
-            return -1;
+        return st.isEmpty() ? -1 : minSt.peek();
+    }
+}
+
+/**
+ * TC: O(1) for all operations, AS: O(n)
+ */
+class MinStack {
+
+    private static Stack<Integer> st = new Stack<>();
+    private static int minEle = Integer.MIN_VALUE;
+
+    static void push(Integer e) {
+        if (e < minEle) {
+            st.push(2 * e - zeroIfMinValue(minEle));
+            minEle = e;
         } else {
-            return min.peek();
+            if (st.isEmpty()) {
+                minEle = e;
+            }
+            st.push(e);
         }
+    }
+
+    static int pop() {
+        if (!st.isEmpty()) {
+            int actTopEle = peek();
+            if (st.peek() < minEle) {
+                minEle = 2 * zeroIfMinValue(minEle) - st.peek();
+            }
+            if (st.isEmpty()) {
+                minEle = Integer.MIN_VALUE;
+            }
+            return actTopEle;
+        }
+        return -1;
+    }
+
+    static int peek() {
+        if (st.isEmpty()) {
+            return -1;
+        }
+        return st.peek() < minEle ? minEle : st.peek();
+    }
+
+    static Integer findMin() {
+        return minEle == Integer.MIN_VALUE ? -1 : minEle;
+    }
+
+    private static int zeroIfMinValue(int ele) {
+        return ele == Integer.MIN_VALUE ? 0 : ele;
     }
 }
