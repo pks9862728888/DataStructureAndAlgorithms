2fdb0d87ed932ae0dbadcbfa9da587ec770bac01
hackedlings <pks9862728888@gmail.com>
2024-06-29 12:35:46 +0530

    Solved some DSA problems

---
+++ src/main/java/com/practice/programs/revise/medium/graph/directed/FindShortestPathFromOneNodeToAllOtherNodesInGraph.java
@@ -0,0 +1,41 @@
+package com.practice.programs.revise.medium.graph.directed;
+
+public class FindShortestPathFromOneNodeToAllOtherNodesInGraph {
+
+    /**
+     * Find shortest distance from 1 node to all other nodes in graph, -1 denotes not reachable
+     * Adjacency matrix is provided in input
+     * TC: O(n^3), AS: O(1)
+     * <a href="https://www.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1">Practice Link</a>
+     * Concepts:
+     * GRAPH
+     * FLOYD_WARSHALLS_ALGORITHM
+     */
+    public void shortestDistance(int[][] matrix) {
+        int n = matrix.length;
+        // replace -1 by infinity to indicate no cycle
+        replaceMatrix(n, matrix, -1, Integer.MAX_VALUE);
+        // Find min cost using floyd warshall algorithm
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][k] != Integer.MAX_VALUE && matrix[k][j] != Integer.MAX_VALUE) {
+                        matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                    }
+                }
+            }
+        }
+        // Do reverse because problem wants -1 in case of no cycle
+        replaceMatrix(n, matrix, Integer.MAX_VALUE, -1);
+    }
+
+    private void replaceMatrix(int n, int[][] matrix, int target, int replacingValue) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == target) {
+                    matrix[i][j] = replacingValue;
+                }
+            }
+        }
+    }
+}
