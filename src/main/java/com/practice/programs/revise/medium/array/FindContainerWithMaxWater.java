22a4ebf591705a98de9660d75153828ebb8aee90
hackedlings <pks9862728888@gmail.com>
2024-08-13 22:07:05 +0530

    Solved some DSA problems

--- src/main/java/com/practice/programs/revise/medium/array/FindContainerWithMaxWater.java
+++ src/main/java/com/practice/programs/revise/medium/array/FindContainerWithMaxWater.java
@@ -1,28 +1,30 @@
 package com.practice.programs.revise.medium.array;
 
 /**
+ * https://leetcode.com/problems/container-with-most-water/
  * TC: O(n), AS: O(1)
- * Method: Two pointer
+ * Concept:
+ * TWO_POINTER
  */
 public class FindContainerWithMaxWater {
 
     // 1 8 6 2 5 4 8 3 7 -> 49 (left most 8 & right most 7 is the container
     // Assume width of bars is negligible and n >= 2
 
-    public int maxArea(int[] height) {
-        int maxArea = 0;
-        int left = 0, right = height.length - 1;
-        while (left < right) {
-            int currArea = (right - left) * Math.min(height[right], height[left]);
-            if (currArea > maxArea) {
-                maxArea = currArea;
-            }
-            if (height[left] > height[right]) {
-                right--;
+    public int maxArea(int[] h) {
+        int low = 0;
+        int high = h.length - 1;
+        int mxArea = 0;
+        while (low < high) {
+            // Calculate max water which can be held
+            mxArea = Math.max(mxArea, Math.min(h[low], h[high]) * (high - low));
+            // Whichever line is low in height, ignore that (because we want max water)
+            if (h[low] <= h[high]) {
+                low++;
             } else {
-                left++;
+                high--;
             }
         }
-        return maxArea;
+        return mxArea;
     }
 }
