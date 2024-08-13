cdc83d8950eeecb9aea0ce5267a48e31b195656a
hackedlings <pks9862728888@gmail.com>
2024-08-13 22:33:36 +0530

    Solved some DSA problems

--- src/main/java/com/practice/programs/easy/arrays/SetMatrixZeros.java
+++ src/main/java/com/practice/programs/easy/arrays/SetMatrixZeros.java
@@ -33,4 +33,60 @@ public class SetMatrixZeros {
         }
     }
 
+    /**
+     * TC: O(m * n), AS: O(1)
+     * https://leetcode.com/problems/set-matrix-zeroes/
+     * Idea is to use first row and column as indicator whether row & column should be marked as 0
+     */
+    public void setZeroesWithoutUsingAuxiliarySpace(int[][] m) {
+        int rowLen = m.length;
+        int colLen = rowLen > 0 ? m[0].length : 0;
+        // Determine which row and column should be marked as zero
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+        for (int row = 0; row < rowLen; row++) {
+            for (int col = 0; col < colLen; col++) {
+                if (m[row][col] == 0) { // mark 0 in first row and first col
+                    m[0][col] = 0;
+                    m[row][0] = 0;
+                    if (row == 0) {
+                        firstRowZero = true;
+                    }
+                    if (col == 0) {
+                        firstColZero = true;
+                    }
+                }
+            }
+        }
+        updateRowNColZeroExclFirstRowNCol(m, rowLen, colLen);
+        updateFirstRowZero(m, firstRowZero, colLen);
+        updateFirstColZero(m, firstColZero, rowLen);
+    }
+
+    private static void updateRowNColZeroExclFirstRowNCol(int[][] m, int rowLen, int colLen) {
+        for (int row = 0; row < rowLen; row++) {
+            for (int col = 0; col < colLen; col++) {
+                if ((row != 0 && col != 0) && (m[row][0] == 0 || m[0][col] == 0)) {
+                    m[row][col] = 0;
+                }
+            }
+        }
+    }
+
+    private static void updateFirstColZero(int[][] m, boolean firstColZero, int rowLen) {
+        if (firstColZero) {
+            for (int row = 0; row < rowLen; row++) {
+                m[row][0] = 0;
+            }
+        }
+    }
+
+    private static void updateFirstRowZero(int[][] m, boolean firstRowZero, int colLen) {
+        if (firstRowZero) {
+            for (int col = 0; col < colLen; col++) {
+                m[0][col] = 0;
+            }
+        }
+    }
+
 }
