d928be746c40233356cb11ca2828db26798f07a1
hackedlings <pks9862728888@gmail.com>
2024-08-13 08:31:52 +0530

    Solved some DSA problems

--- src/main/java/com/practice/programs/medium/arrays/SumOfThreeValuesEqualsK.java
+++ src/main/java/com/practice/programs/medium/arrays/SumOfThreeValuesEqualsK.java
@@ -1,6 +1,10 @@
 package com.practice.programs.medium.arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * TC: O (n * 2 + n log n) = O(n*2), AS: O(1)
@@ -23,7 +27,40 @@ public class SumOfThreeValuesEqualsK {
                 }
             }
         }
-
         return false;
+    }
+
+    /**
+     * https://leetcode.com/problems/3sum/
+     * TC: O(n log n + n ^ 2) = O(n^2)
+     * AS: O(noOfDistinctCombinations)
+     * Concepts:
+     * TWO_POINTER
+     * TWO_POINTER_START_N_END_OF_ARRAY
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<String> cache = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            int low = i + 1;
+            int high = n - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0) {
+                    String key = nums[i] + "#" + nums[low] + "#" + nums[high];
+                    if (cache.add(key)) {
+                        res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                    }
+                    low++;
+                } else if (sum < 0) {
+                    low++;
+                } else {
+                    high--;
+                }
+            }
+        }
+        return res;
     }
 }
