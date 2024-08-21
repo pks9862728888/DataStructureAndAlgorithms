36dc69355231f00242481055baf90795bc909706
hackedlings <pks9862728888@gmail.com>
2024-08-21 07:48:19 +0530

    Solved some DSA problems

--- src/main/java/com/practice/programs/revise/medium/tree/SerializeAndDeserializeBT.java
+++ src/main/java/com/practice/programs/revise/medium/tree/SerializeAndDeserializeBT.java
@@ -1,69 +1,64 @@
 package com.practice.programs.revise.medium.tree;
 
+import com.practice.programs.revise.medium.utils.TreeNode;
+
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
+ * TC: O(n), AS: O(n)
+ * Concepts:
+ * TREE
+ */
 public class SerializeAndDeserializeBT {
 
-    public static String serialize(Node root) {
+    private static final String NULL = "null";
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
         StringBuilder sb = new StringBuilder();
-        Queue<Node> q = new LinkedList<>();
-        if (root != null) {
-            q.add(root);
-        }
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) q.add(root);
         while (!q.isEmpty()) {
-            Node curr = q.poll();
-            if (sb.length() != 0) {
-                sb.append(" ");
-            }
+            TreeNode curr = q.poll();
             if (curr == null) {
-                sb.append("N");
+                append(sb, NULL);
             } else {
-                sb.append(curr.key);
+                append(sb, String.valueOf(curr.val));
                 q.add(curr.left);
                 q.add(curr.right);
             }
         }
-
         return sb.toString();
     }
 
-    public static Node deserialize(String data) {
-        Node root = null;
-        String[] arr = data.split(" ");
-        Queue<Node> q = new LinkedList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (q.isEmpty()) {
-                root = newNode(Integer.parseInt(arr[i]));
-                q.add(root);
-            } else {
-                Node curr = q.poll();
-                String left = arr[i];
-                String right = arr[++i];
-                if (!left.equals("N")) {
-                    curr.left = newNode(Integer.parseInt(left));
-                    q.add(curr.left);
-                }
-                if (!right.equals("N")) {
-                    curr.right = newNode(Integer.parseInt(right));
-                    q.add(curr.right);
-                }
+    private void append(StringBuilder sb, String no) {
+        if (sb.length() > 0) sb.append(",");
+        sb.append(no);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.isEmpty()) return null;
+        String[] nodeValues = data.split(",");
+        int idx = 0;
+        TreeNode root = new TreeNode(Integer.parseInt(nodeValues[idx++]));
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            String leftVal = nodeValues[idx++];
+            String rightVal = nodeValues[idx++];
+            if (!leftVal.equals(NULL)) {
+                curr.left = new TreeNode(Integer.parseInt(leftVal));
+                q.add(curr.left);
+            }
+            if (!rightVal.equals(NULL)) {
+                curr.right = new TreeNode(Integer.parseInt(rightVal));
+                q.add(curr.right);
             }
         }
         return root;
-    }
-
-    static Node newNode(int value) {
-        Node n = new Node();
-        n.key = value;
-        n.left = null;
-        n.right = null;
-        return n;
-    }
-
-    static class Node {
-        int key;
-        Node left;
-        Node right;
     }
 }
