e555c6d0fd8a7766fc5c08a2390233a727606fbb
hackedlings <pks9862728888@gmail.com>
2024-08-27 07:40:01 +0530

    Solved some DSA problems

--- src/main/java/com/practice/programs/revise/medium/greedyalgorithms/JumpGame.java
+++ src/main/java/com/practice/programs/revise/medium/greedyalgorithms/JumpGame.java
@@ -7,7 +7,6 @@ public class JumpGame {
     // 4, 0, 0, 0, 4 -> true
     // 3, 0, 0, 0, 4 -> false
     // Use greedy approach, TC: O(n), AS: O(1)
-
     public String canJump(int[] nums) {
         int currIdx = nums.length - 1;
         for (int prevIdx = currIdx - 1; prevIdx >= 0; prevIdx--) {
@@ -16,5 +15,32 @@ public class JumpGame {
             }
         }
         return String.valueOf(currIdx == 0);
+    }
+
+    /**
+     * https://leetcode.com/problems/jump-game/
+     * From each idx you can jump only upto a fixed length specified by idx[i]
+     * Concept:
+     * DP
+     * BOUNDED_ITERATE_OVER_ALL_POSSIBLE_OPTION_PATTERN
+     */
+    public boolean canJumpII(int[] nums) {
+        Boolean[] cache = new Boolean[nums.length];
+        return canJump(nums, 0, nums.length, cache);
+    }
+
+    private boolean canJump(int[] nums, int idx, int n, Boolean[] cache) {
+        if (idx >= n - 1) {
+            return true;
+        }
+        if (cache[idx] == null) {
+            boolean canReachEnd = false;
+            int jumpLen = nums[idx];
+            for (int jl = 1; jl <= jumpLen; jl++) {
+                canReachEnd = canReachEnd || canJump(nums, idx + jl, n, cache);
+            }
+            cache[idx] = canReachEnd;
+        }
+        return cache[idx];
     }
 }
