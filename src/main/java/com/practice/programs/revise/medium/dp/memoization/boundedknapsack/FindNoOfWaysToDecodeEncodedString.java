b6c31962950a8cdfbcaba977b0ac097b0462cb41
hackedlings <pks9862728888@gmail.com>
2024-08-27 08:12:33 +0530

    Solved some DSA problems

--- src/main/java/com/practice/programs/revise/medium/dp/memoization/boundedknapsack/FindNoOfWaysToDecodeEncodedString.java
+++ src/main/java/com/practice/programs/revise/medium/dp/memoization/boundedknapsack/FindNoOfWaysToDecodeEncodedString.java
@@ -3,6 +3,7 @@ package com.practice.programs.revise.medium.dp.memoization.boundedknapsack;
 import java.util.Arrays;
 
 /**
+ * https://leetcode.com/problems/decode-ways/description/
  * A message containing letters from A-Z is being encoded to numbers using the following mapping:
  * 'A' -> 1
  * 'B' -> 2
@@ -14,42 +15,43 @@ import java.util.Arrays;
  * 0 -> 0
  * TC: O(n)
  * AS: O(n)
- * Pattern: BOUNDED_ZERO_ONE_KNAPSACK
+ * Pattern:
+ * DP
+ * STRING_DP
+ * STRING_DP_WAYS_DECODE
+ * BOUNDED_ZERO_ONE_KNAPSACK
  */
 public class FindNoOfWaysToDecodeEncodedString {
 
-    private static final long MOD = 1000000007;
-
     public int numDecodings(String s) {
-        int n = s.length();
-        long[] cache = new long[n];
+        int strLen = s.length();
+        int[] cache = new int[strLen];
         Arrays.fill(cache, -1);
-        return (int) find(s, n, 0, cache);
+        return numDecodings(s, 0, strLen, cache);
     }
 
-    private long find(String s, int n, int currIdx, long[] cache) {
-        if (currIdx == n) {
+    private int numDecodings(String s, int idx, int n, int[] cache) {
+        if (idx == n) {
             return 1;
         }
-        if (cache[currIdx] == -1) {
-            long waysOneLetter = canSplitByOne(s, currIdx, n) ? find(s, n, currIdx + 1, cache) : 0;
-            long waysTwoLetter = canSplitByTwo(s, currIdx, n) ?
-                    find(s, n, currIdx + 2, cache) : 0;
-            cache[currIdx] = (waysOneLetter + waysTwoLetter) % MOD;
+        if (cache[idx] == -1) {
+            int splitBy1 = canSplitBy1(s, idx, n) ? numDecodings(s, idx + 1, n, cache) : 0;
+            int splitBy2 = canSplitBy2(s, idx, n) ? numDecodings(s, idx + 2, n, cache) : 0;
+            cache[idx] = splitBy1 + splitBy2;
         }
-        return cache[currIdx];
+        return cache[idx];
     }
 
-    private boolean canSplitByOne(String s, int currIdx, int n) {
-        return currIdx < n && s.charAt(currIdx) != '0';
+    private boolean canSplitBy1(String s, int idx, int n) {
+        return idx < n && s.charAt(idx) != '0';
     }
 
-    private boolean canSplitByTwo(String s, int currIdx, int n) {
-        if (currIdx + 1 >= n) {
+    private boolean canSplitBy2(String s, int idx, int n) {
+        if (idx + 1 >= n) {
             return false;
         }
-        char firstLetter = s.charAt(currIdx);
-        char secondLetter = s.charAt(currIdx + 1);
-        return firstLetter != '0' && firstLetter <= '2' && secondLetter <= '6';
+        char firstChar = s.charAt(idx);
+        char secChar = s.charAt(idx + 1);
+        return firstChar == '1' || firstChar == '2' && secChar <= '6';
     }
 }
