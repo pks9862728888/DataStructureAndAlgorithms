4653368e50b73bb53d2e7b13142df27f1d8c5486
pks9862728888 <pks9862728888@gmail.com>
2025-01-04 11:57:12 +0530

    Solved some dsa problems

--- src/main/java/com/practice/programs/revise/medium/greedyalgorithms/ReorganizeStringSuchThatNoTwoSameCharsAppearTogether.java
+++ src/main/java/com/practice/programs/revise/medium/greedyalgorithms/ReorganizeStringSuchThatNoTwoSameCharsAppearTogether.java
@@ -1,89 +1,76 @@
 package com.practice.programs.revise.medium.greedyalgorithms;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
 
+/**
+ * TC: O(n log c) where c is no of distinct chars, so log c can be treated as constant,
+ * So TC becomes O(n)
+ * AS: O(n) for storing result
+ * https://www.educative.io/module/page/AQxxQoOnN1ASnDDZ7pzz6n/5864882417434624/4758112332546048/4712250537541632
+ * Concept:
+ * HEAP
+ * MAX_HEAP
+ */
 public class ReorganizeStringSuchThatNoTwoSameCharsAppearTogether {
 
-    /**
-     * TC: O(n log c) where c is no of distinct chars, so log c can be treated as constant,
-     * So TC becomes O(n)
-     * AS: O(n) for storing result
-     */
     public static String reorganizeString(String str) {
-        PriorityQueue<Pair> pq = buildHeap(str);
-        Queue<Pair> q = new LinkedList<>();
-        StringBuilder reOrganizedStr = new StringBuilder();
-        Character prev = null;
-        while (!pq.isEmpty()) {
-            Optional<Pair> pairOpt = findPairNotEqualToPrev(prev, pq, q);
-            if (!pairOpt.isPresent()) {
-                return "";
+        if (str.isEmpty()) return str;
+
+        // Create PQ (max-heap) to store chars with max frequency and build heap
+        PriorityQueue<Pair> maxHeap = buildFreqMapInPriorityQueue(str);
+
+        // Create sb to store result, but delay insertion
+        StringBuilder resSb = new StringBuilder();
+        Pair prevPair = null;
+        while (!maxHeap.isEmpty() || prevPair != null) {
+            // Base case
+            if (prevPair != null && maxHeap.isEmpty()) {
+                resSb.setLength(0);
+                break;
             }
-            Pair pair = pairOpt.get();
-            reOrganizedStr.append(pair.ch);
-            prev = pair.ch;
-            pair.decrementFreq();
-            if (pair.freq > 0) {
-                pq.add(pair);
-            }
-            addToPQ(pq, q);
+            // Pop next max freq char and add to result
+            Pair currPair = maxHeap.poll();
+            resSb.append(currPair.ch);
+
+            // Add prev max freq char
+            if (prevPair != null) maxHeap.add(prevPair);
+
+            // Decrement curr pair freq but don't add it yet
+            currPair.freq--;
+            prevPair = currPair.freq > 0 ? currPair : null;
         }
 
-        return reOrganizedStr.toString();
+        // Return string result
+        return resSb.toString();
     }
 
-    private static void addToPQ(PriorityQueue<Pair> pq, Queue<Pair> q) {
-        while (!q.isEmpty()) {
-            pq.add(q.poll());
-        }
+    private static PriorityQueue<Pair> buildFreqMapInPriorityQueue(String str) {
+        Map<Character, Integer> freqMap = getCharFreqMap(str);
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>(
+                (a, b) -> -1 * Integer.compare(a.freq, b.freq));
+        freqMap.forEach((key, value) -> maxHeap.add(new Pair(key, value)));
+        return maxHeap;
     }
 
-    private static Optional<Pair> findPairNotEqualToPrev(
-            Character prev, PriorityQueue<Pair> pq, Queue<Pair> q) {
-        while (!pq.isEmpty()) {
-            Pair pair = pq.poll();
-            if (prev == null || !prev.equals(pair.ch)) {
-                return Optional.of(pair);
-            }
-            q.add(pair);
-        }
-        return Optional.empty();
-    }
-
-    private static PriorityQueue<Pair> buildHeap(String str) {
-        Map<Character, Integer> freqMap = buildFreqMap(str);
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        for (Map.Entry<Character, Integer> entry: freqMap.entrySet()) {
-            pq.add(new Pair(entry.getKey(), entry.getValue()));
-        }
-        return pq;
-    }
-
-    private static Map<Character, Integer> buildFreqMap(String str) {
+    private static Map<Character, Integer> getCharFreqMap(String str) {
         Map<Character, Integer> freqMap = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
+        int strLen = str.length();
+        for (int idx = 0; idx < strLen; idx++) {
+            Character ch = str.charAt(idx);
             freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
         }
         return freqMap;
     }
 
-    private static class Pair implements Comparable<Pair> {
-        char ch;
+    private static class Pair {
+        Character ch;
         int freq;
 
-        Pair(char ch, int freq) {
+        public Pair(Character ch, int freq) {
             this.ch = ch;
             this.freq = freq;
-        }
-
-        public void decrementFreq() {
-            this.freq--;
-        }
-
-        @Override
-        public int compareTo(Pair o) {
-            return -1 * Integer.compare(freq, o.freq);
         }
     }
 }
