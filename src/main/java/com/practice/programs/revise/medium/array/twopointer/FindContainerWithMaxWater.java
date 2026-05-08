7925bea18e9ecde693e890656499f653ae9be821
pks9862728888 <pks9862728888@gmail.com>
2026-05-08 22:57:23 +0530

    implemented prototype design pattern

--- src/main/java/com/practice/programs/revise/medium/array/twopointer/FindContainerWithMaxWater.java
+++ src/main/java/com/practice/programs/revise/medium/array/twopointer/FindContainerWithMaxWater.java
@@ -1,5 +1,7 @@
 package com.practice.programs.revise.medium.array.twopointer;
 
+import java.util.ArrayList;
+
 /**
  * https://leetcode.com/problems/container-with-most-water/
  * TC: O(n), AS: O(1)
@@ -11,20 +13,25 @@ public class FindContainerWithMaxWater {
     // 1 8 6 2 5 4 8 3 7 -> 49 (left most 8 & right most 7 is the container
     // Assume width of bars is negligible and n >= 2
 
-    public int maxArea(int[] h) {
-        int low = 0;
-        int high = h.length - 1;
-        int mxArea = 0;
-        while (low < high) {
-            // Calculate max water which can be held
-            mxArea = Math.max(mxArea, Math.min(h[low], h[high]) * (high - low));
-            // Whichever line is low in height, ignore that (because we want max water)
-            if (h[low] <= h[high]) {
-                low++;
-            } else {
-                high--;
-            }
+    public int maxArea(ArrayList<Integer> heights) {
+      int left = 0;
+      int right = heights.size() - 1;
+      int maxWater = 0;
+      while (left < right) {
+        int lh = heights.get(left);
+        int rh = heights.get(right);
+        int water = Math.min(lh, rh) * Math.abs(right - left);
+        maxWater = Math.max(maxWater, water);
+
+        if (lh < rh) {
+          left++;
+        } else if (rh < lh) {
+          right--;
+        } else {
+          left++;
+          right--;
         }
-        return mxArea;
+      }
+      return maxWater;
     }
 }
